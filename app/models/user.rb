eef8ddfe18b11052c5c4dad553e446adb582819f
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-08 11:07:10 +0300

    confirmation by mail at registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  validates :name, presence: true
-  validates :lastName, presence: true
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :posts
 end
