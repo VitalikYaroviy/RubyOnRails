b39cbb08e2d5f468eebc820bd50bc4e5ba8af660
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-09 13:38:50 +0300

    fix bag in opening an application

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: "sessions"}
 
   devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
