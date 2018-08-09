ebca5be57f4e489bc84322ee1a69233147042ec3
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-09 14:13:26 +0300

    setting ActiveAdmin and romove send email this branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,20 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update] # probably want to keep using this
+  before_action :set_user, only: [:show, :edit, :update]
 
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
 
-  # # GET /users/1
-  # # GET /users/1.json
+
   def show
-
   end
 
-  # GET /users/1/edit
+
   def edit
-
   end
 
-  # # PATCH/PUT /users/1
-  # # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -34,12 +28,10 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:role, :user_name)
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,17 +31,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'naprok-task.herokuapp.com', :protocol => 'http' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      user_name:      'vitaliy.y@milestep.io',
-      password:       'MileStep18',
-      domain:         'heroku.com',
-      address:       'smtp.gmail.com',
-      port:          '587',
-      authentication: :plain,
-      enable_starttls_auto: true
-  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
