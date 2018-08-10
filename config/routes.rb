c8176cdf749aec2863dddd8e537d07437b07532e
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-10 12:31:17 +0300

    fix bag in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, :controllers => {:registrations => "registrations"}
-
-  devise_for :users, controllers: {sessions: "sessions"}
+  devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions"}
 
   devise_scope :user do
     authenticated :user do
