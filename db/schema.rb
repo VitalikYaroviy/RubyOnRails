61fe33f7020266258d8fb77ef599daadc59bf355
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-10 16:41:34 +0300

    fix bags for circleCI

--- db/schema.rb
+++ db/schema.rb
@@ -70,19 +70,17 @@ ActiveRecord::Schema.define(version: 2018_08_09_073309) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "lastName"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "name"
-    t.string "lastName"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
