fab7bd1ebd8a313bafc1bb52d5adbb3e641502d9
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-10 09:58:11 +0300

    fix path in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,7 @@
 class SessionsController < Devise::SessionsController
-  def new
-    super
-  end
-
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
-    sign_in(resource_name, resource)
-    if !session[:return_to].blank?
-      redirect_to session[:return_to]
-      session[:return_to] = nil
-    else
-      respond_with resource, :location => after_sign_in_path_for(resource)
-    end
-  end
 
   private
   def after_sign_in_path_for(resource)
-    "/posts"
+    posts_path
   end
 end
