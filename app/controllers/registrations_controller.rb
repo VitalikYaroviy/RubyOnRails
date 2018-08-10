bd97041779c3c11d07765951f4d09ea0b629539c
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-10 13:43:43 +0300

    added path to post after sing up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -27,6 +27,11 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  private
+  def after_sign_up_path_for(resource)
+    posts_path
+  end
+
   def sign_up_params
     params.require(:user).permit(:name, :lastName, :email, :password, :password_confirmation)
   end
