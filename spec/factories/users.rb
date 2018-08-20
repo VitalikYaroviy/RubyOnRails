4490072581ab78eefd1631bc94ee317b8b7ffd2d
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-20 14:14:41 +0300

    fixed code style

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+
   factory :user do
     email {Faker::Internet.email}
     password 'example'
@@ -6,4 +7,5 @@ FactoryBot.define do
     name {Faker::Name.first_name}
     last_name {Faker::Name.last_name}
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe User do
+
   context 'validates the user' do
+
     let(:post) { FactoryBot.build(:user, name: nil, last_name: nil) }
+
     it 'validates the user name  and  last name not empty' do
-      # expect(post.valid?).to be_falsey
       expect(post).to_not be_valid
       expect(post.errors[:name]).to have(1).item
     end
+
   end
 
 end
