3aefbe9a990ad6cdbc84aa1815ee8e5b5c201325
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-21 11:34:12 +0300

    fixed travis

--- db/migrate/20180820103139_remove_last_name_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLastNameFromUsers < ActiveRecord::Migration[5.2]
-  def self.up
-    remove_column :users, :lastName
-  end
-
-  def self.down
-    add_column :users, :last_name, :string
-  end
-end
