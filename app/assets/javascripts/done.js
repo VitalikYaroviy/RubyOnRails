5b89feddf2cd5c0ea0035c53ebda403072bd3d5d
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-28 15:48:17 +0300

    reformat code

--- app/assets/javascripts/done.js
+++ app/assets/javascripts/done.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
-    $("input[type='checkbox']").on('click', function (e) {
-        let copy = Object.assign({}, $(e.target).closest('tr'));
-        $(e.target).closest('tr').detach()
-        if (e.target.checked) {
-            $('.finish').append(copy)
-            $(copy[0]).removeClass('false')
-            $(copy[0]).addClass('true')
+  $("input[type='checkbox']").on('click', function (e) {
+    let copy = Object.assign({}, $(e.target).closest('tr'));
+    $(e.target).closest('tr').detach()
+    if (e.target.checked) {
+      $('.finish').append(copy)
 
-
-        } else if (!e.target.checked) {
-            $('.active').append(copy)
-            $(copy[0]).removeClass('true')
-        }
-    });
+    } else if (!e.target.checked) {
+      $('.active').append(copy)
+    }
+  });
 })
