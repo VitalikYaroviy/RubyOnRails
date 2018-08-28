60aceb60f71a39c3c0b00c44272602a4218eb6b1
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-08-28 14:46:31 +0300

    list tasks and form create task on one page

--- app/assets/javascripts/done.js
+++ app/assets/javascripts/done.js
@@ -1,12 +1,20 @@
-$(document).ready(function () {
-    $("input[type='checkbox']").on('click', function (e) {
-        let copy = Object.assign({}, $(e.target).closest('tr'));
-        $(e.target).closest('tr').detach()
-        if (e.target.checked) {
-            $('.finish').append(copy)
+function Done() {}
 
-        } else if (!e.target.checked) {
-            $('.active').append(copy)
-        }
-    });
-})
+let done = new Done();
+
+Done.prototype.change = function (element) {
+  $(element).on('click', function (e) {
+    let copy = Object.assign({}, $(e.target).closest('tr'));
+    $(e.target).closest('tr').detach();
+    if (e.target.checked) {
+      $('.finish').append(copy)
+
+    } else if (!e.target.checked) {
+      $('.active').append(copy)
+    }
+  });
+};
+
+$(document).ready(function () {
+  done.change($("input.status[type='checkbox']"));
+});
